aae19b2758122a829469369f1931a8b37f878aaf
Felipe Parache <soyfelipepe@gmail.com>
2024-05-07 19:46:19 -0300

    Revert "Creación de enumerados para los juegos de cada consola."
    This reverts commit febda4a96540004d0ce2191084b3f01ed2d3030f.

--- Entidades/Consola.cs
+++ Entidades/Consola.cs
@@ -2,14 +2,6 @@
 {
     public class Consola
     {
-        private string? modelo;
-        private bool conectividadOnline;
-        private double precio;
-        protected List<string> listaVideojuegos;
 
-        public Consola()
-        {
-            listaVideojuegos = new List<string>();
-        }
     }
 }
