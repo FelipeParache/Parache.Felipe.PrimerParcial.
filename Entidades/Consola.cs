da01b579672ded6fc890de65f8665368a96edaa6
Felipe Parache <soyfelipepe@gmail.com>
2024-05-07 23:59:34 -0300

    Creación de enumerados para los juegos de cada consola

--- Entidades/Consola.cs
+++ Entidades/Consola.cs
@@ -2,6 +2,14 @@
 {
     public class Consola
     {
+        private string? modelo;
+        private bool conectividadOnline;
+        private double precio;
+        protected List<string> listaVideojuegos;
 
+        public Consola()
+        {
+            listaVideojuegos = new List<string>();
+        }
     }
 }
